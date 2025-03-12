ff35f4e14bc0c2de76e7b53566c2c3743c8fd829
DevBauke <boef.bosma@gmail.com>
2025-03-12 12:01:30 +0100

    Models: Updated Docent and Student

--- SomerenApp/Models/Docent.cs
+++ SomerenApp/Models/Docent.cs
@@ -2,5 +2,26 @@
 {
     public class Docent
     {
+        int Docentnummer { get; set; }
+        string Voornaam { get; set; }
+        string Achternaam { get; set; }
+        int Leeftijd { get; set; }
+        int Telefoonnummer { get; set; }
+        int Kamernummer { get; set; }
+
+        public Docent(int docentnummer, string voornaam, string achternaam, int leeftijd, int telefoonnummer, int kamernummer)
+        {
+            Docentnummer = docentnummer;
+            Voornaam = voornaam;
+            Achternaam = achternaam;
+            Leeftijd = leeftijd;
+            Telefoonnummer = telefoonnummer;
+            Kamernummer = kamernummer;
+        }
+
+        public Docent()
+        {
+            
+        }
     }
 }
